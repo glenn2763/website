df5835f97245675a9dfef59f4130b61b9ad15acb
Glenn Wysen <gwysen@berkeley.edu>
2020-02-20 17:23:08 -0800

    added good navbar

--- app.js
+++ app.js
@@ -6,3 +6,23 @@ let selectElement = document.getElementById('date')
 selectElement.innerHTML = formatDate
 
 console.log('Here\'s a hidden message')
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function topNavResponsive() {
+    var x = document.getElementById("myTopnav");
+    var bars = document.getElementById("bars");
+    if (x.className === "topnav") {
+        x.classList.add("responsive");
+    } else {
+        x.classList.remove("responsive");
+    }
+}
+
+function topNavColor() {
+    var x = document.getElementById("myTopnav");
+    if (window.pageYOffset > x.height()) {
+        x.className += " scrolled";
+    }
+    else {
+    }
+  };
