a1390dfa6986c0ad348c0ad4e1c348270ce40f7e
Glenn Wysen <gwysen@berkeley.edu>
2020-04-12 21:14:52 -0700

    redesigned whole website

--- app.js
+++ app.js
@@ -59,3 +59,70 @@ function checkButton() {
     }
 }
 
+$(function(){
+    $('.fadein img:gt(0)').hide();
+    setInterval(function(){
+      $('.fadein :first-child').fadeOut()
+         .next('img').fadeIn()
+         .end().appendTo('.fadein');}, 
+      3000);
+});
+
+function change2() {
+    $('.frisbeePictures2 :first-child').fadeOut().next('img').fadeIn().end().appendTo('.frisbeePictures2');
+    console.log('changed 2');
+}
+setTimeout(change2, 2001);
+
+function change3() {
+    $('.frisbeePictures3 :first-child').fadeOut().next('img').fadeIn().end().appendTo('.frisbeePictures3');
+    console.log('changed 3');
+}
+setTimeout(change3, 4001);
+
+function change4() {
+    $('.frisbeePictures4 :first-child').fadeOut().next('img').fadeIn().end().appendTo('.frisbeePictures4');
+    console.log('changed 4');
+}
+setTimeout(change4, 6001);
+
+function cycle1() {
+    $('.frisbeePictures1 img:gt(0)').hide();
+    setInterval(function(){
+      $('.frisbeePictures1 :first-child').fadeOut().next('img').fadeIn().end().appendTo('.frisbeePictures1');}, 8000);
+};
+$(function(){
+    $('.frisbeePictures1 img:gt(0)').hide();
+});
+setTimeout(cycle1, 0);
+
+function cycle2() {
+    $('.frisbeePictures2 img:gt(0)').hide();
+    setInterval(function(){
+      $('.frisbeePictures2 :first-child').fadeOut().next('img').fadeIn().end().appendTo('.frisbeePictures2');}, 8000);
+};
+$(function(){
+    $('.frisbeePictures2 img:gt(0)').hide();
+});
+setTimeout(cycle2, 2000);
+
+function cycle3() {
+    $('.frisbeePictures3 img:gt(0)').hide();
+    setInterval(function(){
+      $('.frisbeePictures3 :first-child').fadeOut().next('img').fadeIn().end().appendTo('.frisbeePictures3');}, 8000);
+};
+$(function(){
+    $('.frisbeePictures3 img:gt(0)').hide();
+});
+setTimeout(cycle3, 4000);
+
+function cycle4() {
+    $('.frisbeePictures4 img:gt(0)').hide();
+    setInterval(function(){
+      $('.frisbeePictures4 :first-child').fadeOut().next('img').fadeIn().end().appendTo('.frisbeePictures4');}, 8000);
+};
+$(function(){
+    $('.frisbeePictures4 img:gt(0)').hide();
+});
+setTimeout(cycle4, 6000);
+
